c6604681f58e8be537c5c45cf69cb799b6bf670b
Yonnick <yonnick00@gmail.com>
2019-07-01 07:24:55 +0000

    Automatically backed up by Learn

--- db/migrate/20190701072023_add_active_to_students.rb
+++ db/migrate/20190701072023_add_active_to_students.rb
@@ -1,5 +1,5 @@
 class AddActiveToStudents < ActiveRecord::Migration[5.0]
   def change
-    add_column :students, :active, :boolean
+    add_column :students, :active, :boolean, :default => true
   end
 end
